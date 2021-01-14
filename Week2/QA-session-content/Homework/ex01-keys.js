29df16fe6b23333e3f8924a3eafb4f43ae7e413a
Saoud <69368480+Oudy94@users.noreply.github.com>
2021-01-14 23:31:53 +0100

    Update ex01-keys.js

--- Week2/QA-session-content/Homework/ex01-keys.js
+++ Week2/QA-session-content/Homework/ex01-keys.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
 const mysql_query = [
 
 //1- Create a table, called Authors. Give it the following fields: (author_no(Primary Key), author_name, university, date_of_birth, h_index, gender)
-"CREATE TABLE authors (author_no INT, author_name VARCHAR(255), university VARCHAR(255), date_of_birth DATE, h_index VARCHAR(255), gender ENUM('male', 'female'), PRIMARY KEY(author_no));",
+"CREATE TABLE authors (author_no INT AUTO_INCREMENT, author_name VARCHAR(255), university VARCHAR(255), date_of_birth DATE, h_index VARCHAR(255), gender ENUM('male', 'female'), PRIMARY KEY(author_no)) CHARACTER SET=utf8;",
 
 //2- Write a query that adds a foreign key column to Authors table that references the column author_no. Call this column Collaborator.
 "ALTER TABLE authors ADD COLUMN collaborator INT AFTER gender, ADD FOREIGN KEY(collaborator) REFERENCES authors(author_no);"
